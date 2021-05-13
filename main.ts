207abaf23e6272388635934bbd28273e05e5db8b
robotescu <80849623+robotescu@users.noreply.github.com>
2021-05-13 16:45:35 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,32 @@
+function masoara_distanta () {
+    dist = TPBot.sonarReturn(TPBot.SonarUnit.Centimeters)
+}
+let dist = 0
 basic.showIcon(IconNames.Tortoise)
 TPBot.headlightColor(0x0000ff)
 basic.forever(function () {
-    if (TPBot.trackLine(TPBot.TrackingState.L_R_line)) {
-        TPBot.setWheels(30, 30)
-    }
-    if (TPBot.trackLine(TPBot.TrackingState.L_unline_R_line)) {
-        TPBot.setWheels(40, 10)
-    }
-    if (TPBot.trackLine(TPBot.TrackingState.L_line_R_unline)) {
-        TPBot.setWheels(10, 40)
-    }
-    if (TPBot.trackLine(TPBot.TrackingState.L_R_unline)) {
-        TPBot.setWheels(0, 0)
+    masoara_distanta()
+    if (dist < 10) {
+        TPBot.stopCar()
+        for (let index = 0; index < 2; index++) {
+            TPBot.headlightColor(0xff0000)
+            basic.pause(500)
+            TPBot.headlightColor(0xffffff)
+            basic.pause(500)
+        }
+        TPBot.headlightColor(0x0000ff)
+    } else {
+        if (TPBot.trackLine(TPBot.TrackingState.L_R_line)) {
+            TPBot.setWheels(20, 20)
+        }
+        if (TPBot.trackLine(TPBot.TrackingState.L_unline_R_line)) {
+            TPBot.setWheels(30, 10)
+        }
+        if (TPBot.trackLine(TPBot.TrackingState.L_line_R_unline)) {
+            TPBot.setWheels(10, 30)
+        }
+        if (TPBot.trackLine(TPBot.TrackingState.L_R_unline)) {
+            TPBot.setWheels(0, 0)
+        }
     }
 })
